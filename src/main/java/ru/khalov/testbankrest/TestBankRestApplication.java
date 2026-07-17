cabd567e9047c65df506feb073bd8fb9af23b773
PavelKhalov <hahaz2@yandex.ru>
2026-07-17 12:37:25 +0300

    first commit

---
+++ src/main/java/ru/khalov/testbankrest/TestBankRestApplication.java
@@ -0,0 +1,13 @@
+package ru.khalov.testbankrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestBankRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestBankRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/khalov/testbankrest/TestBankRestApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.khalov.testbankrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestBankRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
